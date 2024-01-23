c8225e79dc06f79cbdafa294c76c75492af1179f
ohddang <ohddang509@gmail.com>
2024-01-23 15:52:07 +0900

    bug: deploy failed fixed only one component 'export default component'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import SearchBar from "@/components/SearchBar";
 
-export const Page: React.FC = () => {
+const Page: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-5">
       <SearchBar />
