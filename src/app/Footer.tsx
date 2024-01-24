ff808b04e749733c8a21d37ba7333c1a558703e6
ohddang <ohddang509@gmail.com>
2024-01-24 17:46:40 +0900

    footer 입력

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,5 +1,12 @@
 const Footer = () => {
-  return <div className="w-full h-32 bg-black/80"></div>;
+  return (
+    <div className="flex flex-col w-full h-32 bg-black/80 text-white p-5">
+      <span className="flex-grow"></span>
+      <span className="flex flex-grow text-xs items-end">
+        {"Crack.Run provides information Data based on NEXON Open API"}
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
